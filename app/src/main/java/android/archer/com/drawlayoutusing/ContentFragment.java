f8ff883ab35535662dda4449a8666138c31cedbc
Archer <421228286@qq.com>
2016-03-25 21:45:01 +0800

    DrawLayout 的使用。

---
+++ app/src/main/java/android/archer/com/drawlayoutusing/ContentFragment.java
@@ -0,0 +1,28 @@
+package android.archer.com.drawlayoutusing;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Archer on 2016/3/25.
+ */
+public class ContentFragment extends Fragment{
+
+    private TextView mTextView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+       View view=inflater.inflate(R.layout.fragment_content,container,false);
+        //获取View的实例化
+        mTextView= (TextView) view.findViewById(R.id.TextView);
+
+
+        String text=getArguments().getString("text");
+        mTextView.setText(text);
+        return view;
+    }
+}
---
+++ app/src/main/java/android/archer/com/drawlayoutusing/MainActivity.java
@@ -0,0 +1,60 @@
+package android.archer.com.drawlayoutusing;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+
+     private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayList<String> menuLists;
+    private ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList= (ListView) findViewById(R.id.Left_drawer);
+        menuLists=new ArrayList<String>();
+        for (int i=0;i<5;i++){
+            menuLists.add("Button"+i);
+
+            adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menuLists);
+            mDrawerList.setAdapter(adapter);
+            mDrawerList.setOnItemClickListener(this);
+        }
+
+
+
+    }
+
+
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //每当点击一个按钮时，动态插入一个fragment到视图中。
+        Fragment contentFragment =new ContentFragment();
+        Bundle args=new Bundle();
+        args.putString("text", menuLists.get(position));
+        contentFragment.setArguments(args);
+
+
+        FragmentManager fm=getFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame,contentFragment).commit();
+
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+}
